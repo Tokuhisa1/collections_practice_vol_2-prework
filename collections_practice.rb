52e47c87e94c1bd06aced6d03e161e8109ea4479
Winston Tokuhisa <tokuhisawinston@outlook.com>
2019-05-26 10:59:49 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,51 @@
 # your code goes here
+def begins_with_r(tools)
+  tools.each do |tool|
+    if tool[0] != 'r'
+      return false
+    end
+  end
+
+  true
+end
+
+def contain_a(elements)
+  matches = []
+
+  elements.each do |element|
+    if element.include?('a')
+      matches.push(element)
+    end
+  end
+
+  matches
+end
+
+def first_wa(elements)
+  elements.each do |element|
+    if element[0..1] == 'wa'
+      return element
+    end
+  end
+end
+
+def remove_non_strings(elements)
+  strings = []
+
+  elements.each do |element|
+    if element.class == String
+      strings.push(element)
+    end
+  end
+
+  strings
+end
+
+def count_elements(elements)
+  count = []
+  
+  elements.uniq.each do |element|
+    count.push(element)
+    element[:count] = elements.count(element)
+  end
+end
